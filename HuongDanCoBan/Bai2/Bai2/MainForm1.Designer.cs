c8cb718390d2c72aa11fe3ea21892ad4875832db
KEROTAR0 <117642155+KEROTAR0@users.noreply.github.com>
2023-03-13 15:39:33 +0700

    Add files via upload

---
+++ HuongDanCoBan/Bai2/Bai2/MainForm1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Bai2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbNu = new System.Windows.Forms.RadioButton();
+            this.rbNam = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbBlue = new System.Windows.Forms.RadioButton();
+            this.rbRed = new System.Windows.Forms.RadioButton();
+            this.btnChangeColor = new System.Windows.Forms.Button();
+            this.txtColorBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbNu);
+            this.groupBox1.Controls.Add(this.rbNam);
+            this.groupBox1.Location = new System.Drawing.Point(111, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chọn giới tính";
+            // 
+            // rbNu
+            // 
+            this.rbNu.AutoSize = true;
+            this.rbNu.Location = new System.Drawing.Point(6, 64);
+            this.rbNu.Name = "rbNu";
+            this.rbNu.Size = new System.Drawing.Size(39, 17);
+            this.rbNu.TabIndex = 0;
+            this.rbNu.TabStop = true;
+            this.rbNu.Text = "Nữ";
+            this.rbNu.UseVisualStyleBackColor = true;
+            this.rbNu.CheckedChanged += new System.EventHandler(this.rbNu_CheckedChanged);
+            // 
+            // rbNam
+            // 
+            this.rbNam.AutoSize = true;
+            this.rbNam.Location = new System.Drawing.Point(6, 19);
+            this.rbNam.Name = "rbNam";
+            this.rbNam.Size = new System.Drawing.Size(47, 17);
+            this.rbNam.TabIndex = 0;
+            this.rbNam.TabStop = true;
+            this.rbNam.Text = "Nam";
+            this.rbNam.UseVisualStyleBackColor = true;
+            this.rbNam.CheckedChanged += new System.EventHandler(this.rbNam_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtColorBox);
+            this.groupBox2.Controls.Add(this.btnChangeColor);
+            this.groupBox2.Controls.Add(this.rbBlue);
+            this.groupBox2.Controls.Add(this.rbRed);
+            this.groupBox2.Location = new System.Drawing.Point(111, 164);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(296, 107);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chọn màu";
+            // 
+            // rbBlue
+            // 
+            this.rbBlue.AutoSize = true;
+            this.rbBlue.Location = new System.Drawing.Point(6, 56);
+            this.rbBlue.Name = "rbBlue";
+            this.rbBlue.Size = new System.Drawing.Size(83, 17);
+            this.rbBlue.TabIndex = 0;
+            this.rbBlue.TabStop = true;
+            this.rbBlue.Text = "Xanh dương";
+            this.rbBlue.UseVisualStyleBackColor = true;
+            // 
+            // rbRed
+            // 
+            this.rbRed.AutoSize = true;
+            this.rbRed.Location = new System.Drawing.Point(6, 19);
+            this.rbRed.Name = "rbRed";
+            this.rbRed.Size = new System.Drawing.Size(39, 17);
+            this.rbRed.TabIndex = 0;
+            this.rbRed.TabStop = true;
+            this.rbRed.Text = "Đỏ";
+            this.rbRed.UseVisualStyleBackColor = true;
+            // 
+            // btnChangeColor
+            // 
+            this.btnChangeColor.Location = new System.Drawing.Point(95, 34);
+            this.btnChangeColor.Name = "btnChangeColor";
+            this.btnChangeColor.Size = new System.Drawing.Size(75, 23);
+            this.btnChangeColor.TabIndex = 1;
+            this.btnChangeColor.Text = "Đổi màu";
+            this.btnChangeColor.UseVisualStyleBackColor = true;
+            this.btnChangeColor.Click += new System.EventHandler(this.btnChangeColor_Click);
+            // 
+            // txtColorBox
+            // 
+            this.txtColorBox.Location = new System.Drawing.Point(176, 7);
+            this.txtColorBox.Multiline = true;
+            this.txtColorBox.Name = "txtColorBox";
+            this.txtColorBox.ReadOnly = true;
+            this.txtColorBox.Size = new System.Drawing.Size(101, 93);
+            this.txtColorBox.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "Bài 2";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbNu;
+        private System.Windows.Forms.RadioButton rbNam;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtColorBox;
+        private System.Windows.Forms.Button btnChangeColor;
+        private System.Windows.Forms.RadioButton rbBlue;
+        private System.Windows.Forms.RadioButton rbRed;
+    }
+}
+
---
+++ HuongDanCoBan/BaiThucHanhBuoi1/BaiThucHanhBuoi1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiThucHanhBuoi1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // click đôi vào nút chương trình sẽ tự động tạo hàm sự kiện này
+        private void label1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtNameCopy.Text = txtTen.Text;
+        }
+
+        private void txtNameCopy_TextChanged(object sender, EventArgs e)
+        {
+            txtNameCopy.Text = txtTen.Text;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            var tenDangNhap = txtTen.Text;
+            MessageBox.Show($"Xin Chào {tenDangNhap}. Rất vui được gặp bạn", "Thông điệp chào mừng");
+        }
+
+        private void txtTen_TextChanged(object sender, EventArgs e)
+        {
+            txtNameCopy.Text = txtTen.Text;
+        }
+    }
+}
